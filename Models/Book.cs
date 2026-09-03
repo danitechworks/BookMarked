55ce2bd9056e5ece1d054771f05b5412aff3969d
Dannell Bayer <danibayer2003@yahoo.com>
2026-09-03 09:58:22 +0200

    added book, quote and user models

---
+++ Models/Book.cs
@@ -0,0 +1,13 @@
+﻿namespace BookMarked.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; } = string.Empty;
+
+        public string Author { get; set; } = string.Empty;
+
+        public DateOnly PublicationDate { get; set; }
+    }
+}
