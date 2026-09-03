f9e51f035cb85149a279f0952b4b2259e28b6b5b
Dannell Bayer <danibayer2003@yahoo.com>
2026-09-03 10:28:50 +0200

    added book request DTO

---
+++ DTOs/BookRequest.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookMarked.DTOs;
+
+public class BookRequest
+{
+    [Required]
+    [MaxLength(200)]
+    public string Title { get; set; } = string.Empty;
+
+    [Required]
+    [MaxLength(150)]
+    public string Author { get; set; } = string.Empty;
+
+    [Required]
+    public DateOnly PublicationDate { get; set; }
+}
