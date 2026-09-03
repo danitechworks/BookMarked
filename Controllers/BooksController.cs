48b60657afe7f7431c3da557286bd9fd4ae8b2a4
Dannell Bayer <danibayer2003@yahoo.com>
2026-09-03 10:43:05 +0200

    added book controller and removed default weather app files

---
+++ Controllers/BooksController.cs
@@ -0,0 +1,94 @@
+﻿using BookMarked.Data;
+using BookMarked.DTOs;
+using BookMarked.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookMarked.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class BooksController : ControllerBase
+{
+    private readonly AppDbContext _context;
+
+    public BooksController(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Book>>> GetAll()
+    {
+        var books = await _context.Books.ToListAsync();
+
+        return Ok(books);
+    }
+
+    [HttpGet("{id:int}")]
+    public async Task<ActionResult<Book>> GetById(int id)
+    {
+        var book = await _context.Books.FindAsync(id);
+
+        if (book is null)
+        {
+            return NotFound();
+        }
+
+        return Ok(book);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<Book>> Create(BookRequest request)
+    {
+        var book = new Book
+        {
+            Title = request.Title,
+            Author = request.Author,
+            PublicationDate = request.PublicationDate
+        };
+
+        _context.Books.Add(book);
+        await _context.SaveChangesAsync();
+
+        return CreatedAtAction(
+            nameof(GetById),
+            new { id = book.Id },
+            book);
+    }
+
+    [HttpPut("{id:int}")]
+    public async Task<IActionResult> Update(int id, BookRequest request)
+    {
+        var book = await _context.Books.FindAsync(id);
+
+        if (book is null)
+        {
+            return NotFound();
+        }
+
+        book.Title = request.Title;
+        book.Author = request.Author;
+        book.PublicationDate = request.PublicationDate;
+
+        await _context.SaveChangesAsync();
+
+        return NoContent();
+    }
+
+    [HttpDelete("{id:int}")]
+    public async Task<IActionResult> Delete(int id)
+    {
+        var book = await _context.Books.FindAsync(id);
+
+        if (book is null)
+        {
+            return NotFound();
+        }
+
+        _context.Books.Remove(book);
+        await _context.SaveChangesAsync();
+
+        return NoContent();
+    }
+}
