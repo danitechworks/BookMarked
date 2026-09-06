8d6534f468b22bcd0606a8cbf9846f2a32790f10
Dannell Bayer <danibayer2003@yahoo.com>
2026-09-06 10:22:19 +0200

    added sticky navbar

---
+++ client/src/app/components/navbar/navbar.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import {
+  RouterLink,
+  RouterLinkActive
+} from '@angular/router';
+
+@Component({
+  selector: 'app-navbar',
+  imports: [RouterLink, RouterLinkActive],
+  templateUrl: './navbar.html',
+  styleUrl: './navbar.scss'
+})
+export class Navbar {
+  protected menuOpen = false;
+
+  protected toggleMenu(): void {
+    this.menuOpen = !this.menuOpen;
+  }
+
+  protected closeMenu(): void {
+    this.menuOpen = false;
+  }
+}
